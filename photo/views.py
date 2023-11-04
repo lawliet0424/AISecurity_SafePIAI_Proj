00128fd8c6c28edbb5f4b9dc02640d68cada44a0
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2023-11-04 16:52:08 +0900

    Update views.py

--- photo/views.py
+++ photo/views.py
@@ -13,7 +13,9 @@ def upload_file(request):
         form = PhotoForm(request.POST, request.FILES)
         if form.is_valid():
             # file is saved
-            form.save()
+            for file in request.FILES.getlist('image'):
+                # 파일 처리 로직 추가
+                Photo.objects.create(image=file)
             print(request.FILES)
             return redirect('image_list')
     else:
@@ -24,56 +26,30 @@ def image_list(request):
     photos = Photo.objects.all()
     return render(request, 'image_list.html', {'photos': photos})
 
-def download_image(request, pk):
-    photo_instance = get_object_or_404(Photo, pk=pk)
+def download_and_delete_image(request):
 
-    # 이미지 파일 경로를 생성
-    image_path = photo_instance.image.path
+     # 모든 이미지를 가져옵니다. 이 예제에서는 모든 이미지를 가져오지만 필요에 따라 필터링할 수 있습니다.
+    all_images = Photo.objects.all()
 
-    # 파일이 존재하는지 확인
-    if os.path.exists(image_path):
+    # 다운로드 및 삭제된 이미지 수를 추적하는 변수를 초기화합니다.
+    downloaded_images = 0
+
+    for photo_instance in all_images:
+        # 이미지 파일 경로를 가져옵니다.
+        image_path = photo_instance.image.path
+
         with open(image_path, 'rb') as image_file:
-            response = HttpResponse(image_file.read(), content_type='image/png')
+            response = HttpResponse(image_file.read(), content_type='image/jpeg')
             response['Content-Disposition'] = f'attachment; filename="{photo_instance.image.name}"'
-        return response
 
-    else:
-        # 접근 거부 or 파일이 존재하지 않을 경우 404 오류 반환
-        return HttpResponseNotFound('Denied')
+        if os.path.exists(image_path):
+            # 이미지 파일 삭제
+            os.remove(image_path)
 
-def delete_image(request, pk):
-    photo_instance = get_object_or_404(Photo, pk=pk)
+            # 이미지 모델 삭제
+            photo_instance.delete()
 
-    # 이미지 파일 경로를 생성
-    image_path = photo_instance.image.path
+            downloaded_images += 1
 
-    if os.path.exists(image_path):
-        # 이미지 파일 삭제
-        os.remove(image_path)
-
-        # 이미지 모델 삭제
-        photo_instance.delete()
-
-    # 'upload_file' URL로 리다이렉션
-    redirect_url = reverse('upload_file')  
-    # 'upload_file'은 프로젝트의 URL 패턴 이름에 맞게 변경
-    return HttpResponseRedirect(redirect_url)
-
-def download_and_delete_image(request, pk):
-
-    photo_instance = get_object_or_404(Photo, pk=pk)
-
-    # 이미지 다운로드 응답
-    image_path = photo_instance.image.path
-    with open(image_path, 'rb') as image_file:
-        response = HttpResponse(image_file.read(), content_type='image/jpeg')
-        response['Content-Disposition'] = f'attachment; filename="{photo_instance.image.name}"'
-    
-    if os.path.exists(image_path):
-        # 이미지 파일 삭제
-        os.remove(image_path)
-
-        # 이미지 모델 삭제
-        photo_instance.delete()
-    
-    return response
+    # 모든 이미지를 처리한 후 응답을 반환합니다.
+    return HttpResponse(f'Downloaded and deleted {downloaded_images} images.')
